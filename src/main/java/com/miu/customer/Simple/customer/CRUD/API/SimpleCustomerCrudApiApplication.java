0424c8114b7930b2a8ac54a18ec8cc41d8735eb9
hagos <111482114+Kytsync@users.noreply.github.com>
2023-10-16 22:45:14 -0500

    initial set up

---
+++ src/main/java/com/miu/customer/Simple/customer/CRUD/API/SimpleCustomerCrudApiApplication.java
@@ -0,0 +1,13 @@
+package com.miu.customer.Simple.customer.CRUD.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCustomerCrudApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCustomerCrudApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miu/customer/Simple/customer/CRUD/API/SimpleCustomerCrudApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miu.customer.Simple.customer.CRUD.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCustomerCrudApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
